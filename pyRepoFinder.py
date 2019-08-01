b447841201d9633c236b2b535f2da81ae0a7bf93
Vishal Kumar <25267239+vishalx360@users.noreply.github.com>
2019-08-01 18:02:57 +0530

    added advanced script

--- pyRepoFinder.py
+++ pyRepoFinder.py
@@ -1,58 +1,169 @@
 # pyRepoFinder 1.0;
 # This script allows you to get a list of repositories related to a certain language and given search query.;
 # Author : @vishalx360;
-
-
-import requests
+import random
 import dateutil.parser
+import requests
 
 baseUrl = "https://api.github.com/"
 
 # actual search web-api
-# https://api.github.com/search?l=Java&q=java&type=Repositories
+# https://api.github.com/search/repositories?q=stars:>100+pushed:>2019-01-01+followers:>10+language:java
 
 
-payload = {
-    "q": "query",
-    "l": "language",
-    "type": 'Repositories'
-}
+language_list = [
+    "ActionScript",
+    "C",
+    "C#",
+    "C++",
+    "Clojure",
+    "CoffeeScript",
+    "CSS",
+    "Go",
+    "Haskell",
+    "HTML",
+    "Java",
+    "JavaScript",
+    "Lua",
+    "MATLAB",
+    "Objective-C",
+    "Perl",
+    "PHP",
+    "Python",
+    "R",
+    "Ruby",
+    "Scala",
+    "Shell",
+    "Swift",
+    "TeX",
+    "Vim script"
+]
+hashSeperator = "#############################"
+# intro section
+print("\n")
+print(hashSeperator)
+print("----# pyRepoFinder 1.0 #----- ")
+print(hashSeperator)
 
-# getting input for query
-inputQuery = str(input("Type Input query : ") or "java")
-payload['q'] = inputQuery
+
+# q=stars:>100+pushed:>2019-01-01+followers:>10+language:java
+# defaults
+query_stars = '>100'
+query_lastUpdated = '>2019-01-01'
+query_followers = '>10'
+query_language = 'java'
+query_search_keyword = 'web'
+
+
+# START: language input section
+
+# log
+random_languages = ""
+# logging random language
+for i in range(4):
+    random_languages += (random.choice(language_list) + ", ")
+
+print("\n")
+print("----~ Filter By Language ~----- \n")
+print("""
+ ! Popular Languages :-
+     - %s
+ 
+ ! Default : Java""" % random_languages)
+print("\n")
+# log
 
 # getting input for language
-inputLanguage = str(input("Type Language : ") or "Java")
-payload['l'] = inputLanguage
+inputLanguage = str(
+    input(">>> Which Language do you want to filter by ? : ") or "Java")
+query_language = inputLanguage
+# END: language input section
 
+# START: Search keyword section
+# log
+print("\n")
+print("----~ Search-keywords ~-----")
+print("""
+ ! Search-keywords helps to filter repositories
+   that contains given keyword in their name.
+   for example :- Crypto
+
+ ! Default : \"web\" 
+    """)
+# log
+# getting input for query
+inputKeyword = str(input(">>> Repo-Name must contain ? : ")
+                   or query_search_keyword)
+query_search_keyword = inputKeyword
+print("\n")
+# END: Search keyword section
+
+
+# outputFileName input section
+print("----~ Output File name ~-----")
+print("""
+ Specify Name of Output file
+ 
+ ! If file already exists it will be overwritten.
+ ! Only type name of file
+ 
+ Default : output.txt
+
+    """)
 # getting input for outputFile name
-outputFile = str(input("Output txt file : ") or "output")
+outputFile = str(
+    input(">>> What will be the name of output (.txt) file ? : ") or "output")
 
+
+# payload
+queryString = ('stars:%s+pushed:%s+followers:%s+language:%s %s' % (
+    query_stars,
+    query_lastUpdated,
+    query_followers,
+    query_language,
+    query_search_keyword
+))
+
+
+# loging
+print("\n")
+print("Requesting Github API with provided parameters..... ")
+print("\n")
 
 # requesting
-print("\n")
-print("Requesting Github API ..... ")
-
-# accessing API
 searchRepo = requests.get(
-    (baseUrl + "search/repositories"), params=payload, timeout=5)
-
+    (baseUrl + "search/repositories?per_page=100&q=" + queryString), timeout=10)
 
 # converting to JSON
 repoList = searchRepo.json()
 
+# Writing header in output file
+headerText = open((outputFile + '.txt'), 'w')
+headerText.write("""~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+pyRepoFinder 1.0;
+Output for folowing parameters:-
 
-# cleaning output file
-tempOutputFile = open((outputFile + '.txt'), 'w')
-tempOutputFile.write("")
-tempOutputFile.close()
+- Main-language : %s
+- Star-count : %s
+- Followers-count : %s
+- Last-commit by Date Range : %s
+- Search parameter (Optional) : %s
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n
+"""
+                 % (
+                     query_language,
+                     query_stars,
+                     query_followers,
+                     query_lastUpdated,
+                     query_search_keyword)
+                 )
+headerText.close()
 
 
 # counter
 counter = 1
-
 # main loop
+print("Writing to " + (outputFile + '.txt'))
 for item in repoList['items']:
     if (item['stargazers_count'] > 100) & (item["watchers_count"] > 20):
         # using context-manager to open file.
@@ -73,5 +184,5 @@ for item in repoList['items']:
 
 
 # logging details of request
-print("Found %d Repositories and saved in %s" %
-      ((counter - 1), (outputFile + '.txt')))
+print("Found %d Repositories  and saved in %s" %
+      ((counter - 1), (outputFile + '.txt\n')))
